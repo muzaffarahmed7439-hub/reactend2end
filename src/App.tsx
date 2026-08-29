b56814ca41a78dca6f44024095b5aad0ac7908ba
muzaffarahmed7439-hub <muzaffarahmed7439@gmail.com>
2026-08-29 23:29:30 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <>
       <section id="center">
         <div>
-          <h1>Hello Muzaffar</h1>
+          <h1>Hello Muzaffar Ahmed</h1>
         </div>
       </section>
     </>
