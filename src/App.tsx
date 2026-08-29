f29e25f644a939bfcdbafe8e92541612a482b26f
muzaffar ahmed <muzaffarahmed7439@gmail.com>
2026-08-29 17:01:50 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>Hello World</h1>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
